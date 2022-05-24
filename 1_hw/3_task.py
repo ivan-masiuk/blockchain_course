87d95931631da535751973538c3eb881ef011b3e
ivan_masiuk <vavolan@gmail.com>
2022-05-24 13:22:13 +0300

    add requirements to task_3

--- 1_hw/3_task.py
+++ 1_hw/3_task.py
@@ -1,24 +1,23 @@
 import string
-import timeit
+import time
 
 letters = string.ascii_lowercase
 numbers = '0123456789'
 
 
 def addCheckRemove(test_key, added_sym, _key):
+    statement = False
     test_key += added_sym
     if test_key == _key:
-        print('YES')
         statement = True
     else:
-        statement = False
         test_key = test_key[:-1]
     return test_key, statement
 
 
 def findKey(example_key_8_bit='0xcf'):
     main_key_start = "0x"
-
+    start_time = time.monotonic()
     statement = False
 
     for num in numbers:
@@ -41,7 +40,9 @@ def findKey(example_key_8_bit='0xcf'):
                 for let_1 in letters:
                     test_key, statement = addCheckRemove(test_key, let_1, example_key_8_bit)
                     if statement: break
+    end_time = time.monotonic()
+    return (end_time - start_time) * 1000
 
 
 if __name__ == '__main__':
-    print("Average time function: ", timeit.timeit(findKey, number=10))
+    print(findKey(), "m-seconds")
